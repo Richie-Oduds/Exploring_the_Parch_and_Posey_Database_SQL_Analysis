060dc5cede22e8d2aefa43fa5ba6d11c265f04fc
Odudu Ochuko Richie <oduduochukorichie@gmail.com>
2024-05-03 09:12:21 +0100

    Delete Screenshots for the Data Cleaning & Analysis/Data Cleaning 1.sql

--- Screenshots for the Data Cleaning & Analysis/Data Cleaning 1.sql
+++
@@ -1,9 +0,0 @@
-/* In the accounts table, there is a column holding the website for each company. The last three digits specify 
-what type of web address they are using.Pull these extensions and provide how many of each website type exist
-in the accounts table. */
-
-SELECT RIGHT(website, 3) AS domain, COUNT(*) num_companies
-FROM accounts
-GROUP BY 1
-ORDER BY 2 DESC
-
