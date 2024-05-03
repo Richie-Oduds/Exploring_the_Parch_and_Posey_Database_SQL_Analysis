4c689b272a8eecdae1008888da545d72ce65e7bb
Odudu Ochuko Richie <oduduochukorichie@gmail.com>
2024-05-03 09:16:36 +0100

    Delete Screenshots for the Data Cleaning & Analysis/Data Cleaning 6.sql

--- Screenshots for the Data Cleaning & Analysis/Data Cleaning 6.sql
+++
@@ -1,6 +0,0 @@
-/* Now see if you can do the same thing for every rep name in the sales_reps table. Again provide 
-first and last name columns. */
-
-SELECT LEFT(name, STRPOS(name, ' ')-1) AS first_name,
-       RIGHT(name, LENGTH(name)- STRPOS(name, ' ')) AS last_name
-FROM sales_reps;
