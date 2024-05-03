0af843891450be46256e7ef0ec5c1905a20f9aa8
Odudu Ochuko Richie <oduduochukorichie@gmail.com>
2024-05-03 09:12:48 +0100

    Delete Screenshots for the Data Cleaning & Analysis/Data Cleaning 2.sql

--- Screenshots for the Data Cleaning & Analysis/Data Cleaning 2.sql
+++
@@ -1,8 +0,0 @@
-/* There is much debate about how much the name (or even the first letter of a company name) matters. Use the accounts 
-table to pull the first letter of each company name to see the distribution of company names 
-that begin with each letter (or number). */
-
-SELECT LEFT(UPPER(name), 1) AS first_letter, COUNT(*) num_companies
-FROM accounts
-GROUP BY 1
-ORDER BY 2 DESC;
